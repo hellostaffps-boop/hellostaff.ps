0b26dc1cae89554487e851d876a440d0bc829e54
Hello Barista <hello.baristahub@gmail.com>
2026-04-09 12:22:18 +0000

    File changes

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -17,6 +17,10 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
 export const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
 
 export default app;
