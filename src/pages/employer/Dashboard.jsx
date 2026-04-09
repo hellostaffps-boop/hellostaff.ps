f539df5edb5b44ea8272d556290ec53c452106ac
Hello Barista <hello.baristahub@gmail.com>
2026-04-09 22:12:36 +0000

    File changes

--- src/pages/employer/Dashboard.jsx
+++ src/pages/employer/Dashboard.jsx
@@ -8,8 +8,8 @@ import EmptyState from "../../components/EmptyState";
 import ProfileCompletionCard from "../../components/ProfileCompletionCard";
 import { useLanguage } from "@/hooks/useLanguage";
 import { useFirebaseAuth } from "@/lib/firebaseAuth";
-import { getEmployerProfile, getEmployerOrganizationJobs, getApplicationsByOrg, getOrganization, getApplicationEvaluation, getEmployerHiringReviewSummary } from "@/lib/firestoreService";
 import { useQuery } from "@tanstack/react-query";
+import { getEmployerProfile, getEmployerOrganizationJobs, getApplicationsByOrg, getOrganization, getApplicationEvaluation, getEmployerHiringReviewSummary } from "@/lib/firestoreService";
 import { getOrganizationMemberCount } from "@/lib/teamService";
 import { getOrgCompletion } from "@/lib/profileCompletion";
 
